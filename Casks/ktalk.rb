01effc6239b285641c15994b641bf1972a57004f
toogle <tooogle@mail.ru>
2024-10-25 20:34:16 +0300

    ktalk 2.13.0

--- Casks/ktalk.rb
+++ Casks/ktalk.rb
@@ -1,6 +1,6 @@
 cask "ktalk" do
-  version "2.12.0"
-  sha256 "3648fe408f23742d36a833bfee5d1bad6c011c98373e7071d702b35f3452330d"
+  version "2.13.0"
+  sha256 "24aef3978fa5bdf7822a585584bb6423527297ae84b3fc5970379fd51b3e16cd"
 
   url "https://st.ktalk.host/ktalk-app/mac/ktalk.#{version}-mac.dmg"
   name "ktalk"
