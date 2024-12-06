004813b4b4e98d3ed8c7e72638ec706c084a6129
toogle <tooogle@mail.ru>
2024-12-06 19:16:57 +0300

    boardmix 1.6.5

--- Casks/boardmix.rb
+++ Casks/boardmix.rb
@@ -1,13 +1,13 @@
 cask "boardmix" do
-  version "1.5.3"
-  sha256 "b329f1face679db5660b3f800a1fc97bfe20a02a4bbfd3369a83f8507949ba78"
+  version "1.6.5"
+  sha256 "0f29a884127db8b6bb8d8b53fbdf3fa754d13fa1cdacd23afc887814137c2faf"
 
   url "https://boardmix-cms-ff.oss-eu-central-1.aliyuncs.com/packages/download/desktop/bosyun/#{version}/boardmix_mac_#{version}_universal.dmg"
   name "boardmix"
   desc "Boardmix"
   homepage "https://boardmix.com"
 
-  app "Boardmix.app"
+  app "boardmix-intl.app"
 
   livecheck do
     url "https://boardmix.com/download"
